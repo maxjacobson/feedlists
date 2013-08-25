eadf080bb833f5d5511837d49ac93a254be2049d
Max Jacobson <maxwell.jacobson@gmail.com>
2013-08-25 07:36:52 -0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Feedlists::Application.config.secret_token = 'bda16a0a7d44fd6bc1b86bb3c97629ae8c60d662dae9ead1f37116998b175f0280815582b617e6955bcc928d21f6ab413735875f859db3e2383a41ca51f408a7'
