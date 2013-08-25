0c2083b23c235b82d2555acdc68938a20f72c232
Max Jacobson <maxwell.jacobson@gmail.com>
2013-08-25 19:23:24 -0400

    adds a basic validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :password
+  validates_uniqueness_of :email
+  validates_presence_of :email, :password
   CIPHER = Gibberish::AES.new(ENV["GIBBERISH"])
 
   def subscriptions
