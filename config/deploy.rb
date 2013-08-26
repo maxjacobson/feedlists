c694ca18de24973f457131553a56bac360fbc2b2
Max Jacobson <maxwell.jacobson@gmail.com>
2013-08-26 18:53:21 -0400

    adds symlink and calls symlink_config on every deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,6 +14,8 @@ default_run_options[:pty] = true
 role :web, "192.241.250.92"    # Your HTTP server, Apache/etc
 role :app, "192.241.250.92"    # This may be the same as your `Web` server
 
+before "deploy:assets:precompile", "deploy:symlink_config"
+
 # if you want to clean up old releases on each deploy uncomment this:
 # after "deploy:restart", "deploy:cleanup"
 
@@ -26,5 +28,9 @@ namespace :deploy do
  task :stop do ; end
  task :restart, :roles => :app, :except => { :no_release => true } do
    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+ end
+ task :symlink_config, :roles => :app do 
+   run "ln -nfs #{shared_path}/production.sqlite3 #{current_release}/db/production.sqlite3"
+   run "ls -nfs #{shared_path}/application.yml #{current_release}/config/application.yml"
  end
 end
